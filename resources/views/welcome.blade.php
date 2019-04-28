8a6f0ed9992dff16495e1eab505abf93a775a945
renzhifan <zhifan6797@163.com>
2019-04-28 14:36:00 +0800

    去掉登录按钮

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,19 +72,19 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
+                        {{--@if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
-                        @endif
+                        @endif--}}
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                请先登录
                 </div>
 
-                <div class="links">
+                {{--<div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -92,7 +92,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                </div>--}}
             </div>
         </div>
     </body>
