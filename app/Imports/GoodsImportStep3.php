e6896ced0309038e177adf95bca436c547b29bac
renzhifan <zhifan6797@163.com>
2019-05-08 10:38:21 +0800

    支付宝订单处理

--- app/Imports/GoodsImportStep3.php
+++ app/Imports/GoodsImportStep3.php
@@ -13,16 +13,32 @@ class GoodsImportStep3 implements ToArray
      */
     public function array(array $row)
     {
+        unset($row[0]);
+        unset($row[1]);
+        unset($row[2]);
         dd($row);
-        /*foreach ($row as $val) {
-            if(!empty(Goods::where('order_number',' ' . trim($val[13]) . ' ')->first())){
-                Goods::updateStep2(' ' . trim($val[13]) . ' ',
+        foreach ($row as $val) {
+            if(!empty(Goods::where('payment_number',trim($val[2]))->first())){
+                $payment_method='';
+                $payment_date_two='';
+                $refund='';
+                if(strpos($val[10],'信用卡')!== false){
+                    $payment_method='信用卡';
+                }
+                if($val[5]==' ' && $val[13]=='*昊'){
+                    $payment_date_two=$val[1];
+                    $refund=$val[6];
+                }
+
+                Goods::updateGoods(' ' . trim($val[13]) . ' ',
                     [
-                        'accounting_time_two' => $val[2],
+                        'payment_method' =>$payment_method,
+                        'payment_date_two' =>$payment_date_two,
+                        'refund' =>$refund,
                         'taobao_customer_return_point' => $val[9],
                         'service_fee_for_head_of_regiment' => $val[11],
                     ]);
             }
-        }*/
+        }
     }
 }
